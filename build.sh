c25c5880fd176442d642ec6c4d8f71c3c87421fd
Wei-Sheng Chin <wschin@outlook.com>
2021-01-20 05:59:11 +0000

    Clean

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 PYTORCH_DIR=$(python -c 'import os, torch; print(os.path.dirname(os.path.realpath(torch.__file__)))')
-echo ${PYTORCH_DIR}
 mkdir -p build && cd build
 cmake .. -DPYTORCH_DIR=${PYTORCH_DIR}
 make -j 24
